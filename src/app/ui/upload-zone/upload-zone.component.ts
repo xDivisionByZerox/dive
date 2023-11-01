792805642239cecea06573d2086d5256094e4f38
xDivisionByZerox <leyla.jaehnig@gmx.de>
2023-11-01 23:55:25 +0100

    refactor(UploadZoneComponent): handle files

--- src/app/ui/upload-zone/upload-zone.component.ts
+++ src/app/ui/upload-zone/upload-zone.component.ts
@@ -1,6 +1,6 @@
 import { Component, HostListener } from '@angular/core';
+import { Subject, filter, fromEvent, map, scan, take } from 'rxjs';
 import { IconComponent } from '../icon/icon.component';
-import { fromEvent, take } from 'rxjs';
 
 @Component({
   selector: 'app-upload-zone',
@@ -16,8 +16,35 @@ export class UploadZoneComponent {
     input.type = 'file';
     input.accept = 'image/*';
 
-    fromEvent(input, 'change').pipe(take(1)).subscribe(console.log);
+    fromEvent(input, 'change')
+      .pipe(
+        take(1),
+        map(() => input.files)
+      )
+      .subscribe((files) => this.filesToAdd$.next(files));
 
     input.click();
   }
+
+  @HostListener('dragover', ['$event'])
+  onDragOver(event: DragEvent) {
+    // prevent default to browser from handling the drop event
+    event.preventDefault();
+  }
+
+  @HostListener('drop', ['$event'])
+  handleDrop(event: DragEvent) {
+    // prevent default to browser from handling the drop event
+    event.preventDefault();
+
+    this.filesToAdd$.next(event.dataTransfer?.files);
+  }
+
+  private readonly filesToAdd$ = new Subject<FileList | undefined | null>();
+
+  readonly files$ = this.filesToAdd$.pipe(
+    map((fileList) => Array.from(fileList ?? { length: 0 })),
+    filter((files) => files.length > 0),
+    scan((previous, current) => [...previous, ...current])
+  );
 }
